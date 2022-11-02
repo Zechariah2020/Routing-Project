b8852ce8cfe09f22ab062a76810fa5c534a275ca
Charlotte!2023 <MichaelScofield2015@Yahoo.Com>
2022-11-02 18:40:17 +0330

    Added Seeder, Login Route, and Authorization

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,8 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Admin;
+use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
 {
     //
+    function login(Request $request)
+    {
+        $user = Admin::where('email', $request->email)->first();
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response([
+                'message' => ['These credentials do not match our records.']
+            ], 404);
+        }
+
+        $token = $user->createToken('my-app-token')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,8 +20,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    //All secure URLs
+    Route::post("add", [ProductController::class, "add"]);
+});
+
 Route::get("ShowAllProducts", [ProductController::class, "ShowAllProducts"]);
 Route::get("list/{id}", [ProductController::class, "list"]);
-Route::post("add", [ProductController::class, "add"]);
 Route::put("update", [ProductController::class, "update"]);
 Route::delete("delete/{id}", [ProductController::class, "delete"]);
+
+Route::post("login", [AdminController::class, "login"]);
