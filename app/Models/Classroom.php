60580a20316646717c1f4a4ae765962abe9d8910
Charlotte!2023 <MichaelScofield2015@Yahoo.Com>
2022-11-09 18:11:30 +0330

    Created many to many relationship between the classrooms and the students tables

--- app/Models/Classroom.php
+++ app/Models/Classroom.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Classroom extends Model
 {
     use HasFactory;
+
+    public function students()
+    {
+        return $this->belongsToMany(Student::class);
+    }
 }
